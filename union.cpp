af823d1bb3dfca4c4e5ea849d77b845452eb6ed1
Ubuntu <ubuntu@ip-172-31-23-56.us-east-2.compute.internal>
2018-11-07 11:23:03 +0000

    union

---
+++ union.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace std;
+
+int main()
+{
+int n[10];
+for(int i=0;i<10;i++)
+{
+cin>>n[i];
+}
+
+
+//union(2,3);
+//union(3,5);
+//connected(2,5);
+//cout<<"union";
+
+}
